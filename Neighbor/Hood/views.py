86d96c939b01146625e3a2582a01bd08cea4f3c0
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-15 18:57:03 +0300

    index route working

--- Neighbor/Hood/views.py
+++ Neighbor/Hood/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    hi = "Hi all!"
+    content = {
+        "hi": hi
+    }
+    return render(request, 'home.html', content)
